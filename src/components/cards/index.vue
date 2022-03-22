7333cec153341295e67e75646a00e25a3c43a078
isaidakbar <smahmudxojaev@gmail.com>
2022-03-22 18:56:34 +0500

    modifies

---
+++ src/components/cards/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <template>
+      <v-card class="mx-auto" max-width="100%">
+        <v-img
+          :src="element.image"
+          :lazy-src="require('../../assets/loader.jpg')"
+          height="200px"
+        ></v-img>
+        <v-row align="center" :gutters="[10, 10]" justify="space-between">
+          <v-col cols="auto">
+            <v-card-title class="overflow-hidden"> {{element.title }} </v-card-title>
+            <v-card-subtitle
+              ><span> {{ element.description}} </span>
+            </v-card-subtitle>
+          </v-col>
+          <v-col cols="auto">
+            <v-sheet color="blue lighten-2" class="px-2 py-2 rounded-lg text-body-2 mx-2">
+              {{ element.delevery_time }}
+            </v-sheet>
+          </v-col>
+        </v-row>
+        <v-card-actions>
+            <v-row>
+                <v-col cols="12" class="py-0">
+                    <v-divider style="  border-top: 0.0625rem dashed rgba(32, 33, 37, 0.12)"></v-divider>
+                </v-col>
+                <v-col class="text--primary">
+                    <v-icon left>mdi-bike</v-icon> 23 000 <small>uzs</small>
+                </v-col>
+            </v-row>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['element'],
+  data () {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,32 +1,85 @@
 <template>
-  <v-row no-gutters class="my-4">
-    <v-col cols="auto">
-      <v-btn text>Malta ></v-btn>
+  <div>
+    <v-row no-gutters class="my-4">
+    <v-col cols="4">
+      <v-btn text>Malta <v-icon right>mdi-chevron-down</v-icon></v-btn>
     </v-col>
-    <v-spacer></v-spacer>
     <v-col cols="auto">
       <v-row no-gutters justify="center">
         <v-col v-for="item in btnLink" :key="item.link" cols="auto" class="mx-1">
-          <v-btn text class="rounded-xl" :to="item.link" active-class="background blue darken-1 white--text"> <v-icon left> {{ item.icon }}</v-icon>{{ item.title }}</v-btn>
+          <v-btn text class="rounded-xl text-capitalize" :to="item.link" active-class="background blue darken-1 white--text"> <v-icon left> {{ item.icon }}</v-icon>{{ item.title }}</v-btn>
         </v-col>
       </v-row>
     </v-col>
         <v-spacer></v-spacer>
   <v-col cols="auto">
-      <v-btn text>Malta ></v-btn>
     </v-col>
   </v-row>
+  <v-row>
+    <v-col v-for="item in restaurants" :key="item" cols="4">
+      <Card :element="item" >hello</Card>
+    </v-col>
+  </v-row>
+  </div>
 </template>
 
 <script>
+import Card from '../../components/cards'
 export default {
+  components: {
+    Card
+  },
   data () {
     return {
+      restaurants: [
+        {
+          url: '',
+          title: "McDonald's Sliema",
+          description: "I'm lovin' it! Minimum order 15€",
+          image: 'https://imageproxy.wolt.com/venue/5e874c0da63b1e40831cb39b/2783d026-77eb-11ea-b3e5-0a58647e8cba_1010-x-544.jpg',
+          delevery_time: '15 min ~ 20 min'
+        },
+        {
+          url: '',
+          title: 'KFC Gzira',
+          description: "Finger lickin' good.",
+          image: 'https://imageproxy.wolt.com/venue/5f9812bfdc6e1970d31c866a/0cd2c572-1f78-11eb-8ed9-c6a78ec5cb8a_wolt_banner_strips.jpg',
+          delevery_time: '15 min ~ 25 min'
+        },
+        {
+          url: '',
+          title: "J'Oli Sandwich and Salad",
+          description: 'Delicious healthy food',
+          image: 'https://imageproxy.wolt.com/venue/5e6fa61d14bcaedc41e84673/1855e6ca-a1f3-11ea-bdd8-0a5864764224_9_daryl_cauchi_dsc_7535.jpg',
+          delevery_time: '20 min ~ 30 min'
+        },
+        {
+          url: '',
+          title: "Moo's Kebab Gzira",
+          description: 'Turkish delicacies!',
+          image: 'https://imageproxy.wolt.com/venue/5e7c615ef14d33ada8c0860c/0055942a-bf5e-11ea-883a-223c443d7624_118_daryl_cauchi_9328.jpg',
+          delevery_time: '21 ~ 30 min'
+        },
+        {
+          url: '',
+          title: 'Pad Thai Kitchen',
+          description: 'Previously Good Thaimes at Ju',
+          image: 'https://imageproxy.wolt.com/venue/5ed1251a79f6b34a1399f7e3/fbe4be7e-d335-11eb-a9c4-fe525f04ec15_pad_thai_kitchen_2.jpg',
+          delevery_time: '21 ~ 30 min'
+        },
+        {
+          url: '',
+          title: "Moo's Kebab Gzira",
+          description: 'Turkish delicacies!',
+          image: 'https://imageproxy.wolt.com/venue/5e7c615ef14d33ada8c0860c/0055942a-bf5e-11ea-883a-223c443d7624_118_daryl_cauchi_9328.jpg',
+          delevery_time: '21 ~ 30 min'
+        }
+      ],
       btnLink: [
         {
           link: '1',
           title: 'Discovery',
-          icon: 'mdi-power-socket-it'
+          icon: 'mdi-map-marker-distance'
         },
         {
           link: '/',
