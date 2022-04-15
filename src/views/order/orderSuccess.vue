5b84f86b1aced2247da476d8438e0e7bfc3da46c
isaidakbar <smahmudxojaev@gmail.com>
2022-04-15 14:34:08 +0500

    modifies el

--- src/views/order/orderSuccess.vue
+++ src/views/order/orderSuccess.vue
@@ -1,5 +1,6 @@
+
 <template>
-  <v-row justify="center" class="fireworks-container">
+  <v-row justify="center" class="fireworks-container styler" id="mytest1">
     <v-col cols="auto" class="mt-10">
       <v-card color="white" class="rounded-lg pa-2" height="auto" width="441">
           <v-card-text class="">
@@ -23,8 +24,7 @@
 </template>
 
 <script>
-import { Fireworks } from 'fireworks-js'
-
+// import { Fireworks } from 'fireworks-js'
 export default {
   computed: {
     getOrderNumber () {
@@ -35,19 +35,16 @@ export default {
     this.$store.commit('CLEAR_PRODUCT')
   },
   mounted () {
-    const container = document.querySelector('.fireworks-container')
-    const fireworks = new Fireworks(container, { /* options */ })
-    fireworks.start()
-    // fireworks.pause()
-    // fireworks.clear()
-    // // stop and clear fireworks
-    // fireworks.stop()
-    // after initialization you can change the fireworks parameters
-    fireworks.setOptions({
-      delay: { min: 10, max: 15 },
-      acceleration: 1.06,
-      explosion: 10
-    })
+    setTimeout(() => {
+      // eslint-disable-next-line no-undef
+      party.confetti(document.getElementById('mytest1'))
+      // eslint-disable-next-line no-undef
+      party.confetti(document.getElementById('mytest1'))
+      // eslint-disable-next-line no-undef
+      party.confetti(document.getElementById('mytest1'))
+      // eslint-disable-next-line no-undef
+      party.confetti(document.getElementById('mytest1'))
+    }, 300)
   }
 }
 </script>
